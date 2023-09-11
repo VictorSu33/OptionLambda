85b07042fc3105c11449725816f3cf55f21e32b1
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-09-11 11:08:48 -0400

    update

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ class Data():
         self.ib.connect('127.0.0.1', 7496, clientId=1)
 
 
-    def create_contract(self, symbol :str):
+    def create_chain(self, symbol :str, date: str, strike_range: tuple, right: str):
         stock = Stock(symbol, 'SMART', 'USD')
         self.ib.qualifyContracts(stock)
+        chains = self.ib.reqSecDefOptParams(stock.symbol, '', stock.secType, stock.conId)
+        chain = next(c for c in chains if c.exchange == 'SMART')
+        price = self.get_stock_price(stock)
+        strikes = [strike for strike in chain.strikes
+            if strike_range[0] < strike < strike_range[1]]
+        contracts = [Option(stock, date, strike, right, 'SMART')
+            for strike in strikes]
+        return contracts
+
+        
+    def get_stock_price(self, stock: Contract):
         data1 = self.ib.reqMktData(stock)
         self.ib.sleep(0.5)
         price = data1.marketPrice()
         self.ib.cancelMktData(stock)
         return price
         
+        
 test = Data()
-print(test.create_contract('SPY'))
+print(util.df(test.create_chain('SPY', '20230911', (400, 500), 'C')))
