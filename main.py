3fea95f7d8a84565ec31eaaccbfc30f78614898d
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-09-24 22:33:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 from ib_insync import *
 util.startLoop()
 
-class Data():
-    def __init__(self) -> None:
-        self.ib = IB()
-        self.ib.connect('127.0.0.1', 7496, clientId=1)
+ib = IB()
+ib.connect('127.0.0.1', 7496, clientId=1)
+ib.reqMarketDataType(4)
 
+def get_stock_price(stock: Contract):
+    data1 = ib.reqMktData(stock)
+    ib.sleep(0.5)
+    price = data1.marketPrice()
+    ib.cancelMktData(stock)
+    return price
 
-    def create_chain(self, symbol :str, date: str, strike_range: tuple, right: str):
-        stock = Stock(symbol, 'SMART', 'USD')
-        self.ib.qualifyContracts(stock)
-        chains = self.ib.reqSecDefOptParams(stock.symbol, '', stock.secType, stock.conId)
-        chain = next(c for c in chains if c.exchange == 'SMART')
-        price = self.get_stock_price(stock)
-        strikes = [strike for strike in chain.strikes
-            if strike_range[0] < strike < strike_range[1]]
-        contracts = [Option(stock, date, strike, right, 'SMART')
-            for strike in strikes]
-        return contracts
-
+def create_chain(symbol :str, date: str, strike_range: tuple, right: str):
+    stock = Stock(symbol, 'SMART', 'USD')
+    ib.qualifyContracts(stock)
+    chains = ib.reqSecDefOptParams(stock.symbol, '', stock.secType, stock.conId)
+    chain = next(c for c in chains if c.exchange == 'SMART')
+    strikes = [strike for strike in chain.strikes
+        if strike_range[0] < strike < strike_range[1]]
+    contracts = [Option(stock, date, strike, right, 'SMART')
+        for strike in strikes]
+    return contracts
         
-    def get_stock_price(self, stock: Contract):
-        data1 = self.ib.reqMktData(stock)
-        self.ib.sleep(0.5)
-        price = data1.marketPrice()
-        self.ib.cancelMktData(stock)
-        return price
-        
-        
-test = Data()
-print(util.df(test.create_chain('SPY', '20230911', (400, 500), 'C')))
+print(util.df(create_chain('SPY', '20230911', (400, 500),'C')))
