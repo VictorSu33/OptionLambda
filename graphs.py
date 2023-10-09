661c277809c7675c88294e5c38890009d4988caa
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-10-08 22:46:28 -0400

    graphs update

--- graphs.py
+++ graphs.py
@@ -1,1 +1,18 @@
+from ib_insync import *
+from optiondata import OptionData
+import matplotlib.pyplot as plt
 
+class Graphs():
+
+    _data: list[OptionData]
+
+    def __init__(self, option_set: list[Option]):
+        _data = [OptionData(option) for option in option_set]
+
+    def plot_delta(self):
+        x = [option.getstrike() for option in self._data]
+        y = [option.getgreeks[0] for option in self._data]
+
+        plot = plt.plot(x,y)
+
+        plt.show()
